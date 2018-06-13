371cbbf1ea7209226c8b816c1a6405a0d8497828
Ahmad Hobby <ahmad.hobby@gmail.com>
2018-06-12 22:04:28 -0400

    Initial Commit

---
+++ Storyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Storyboard
+//
+//  Created by Ahmad Hobby on 2018-06-12.
+//  Copyright © 2018 Ahmad Hobby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Storyboard
+//
+//  Created by Ahmad Hobby on 2018-06-12.
+//  Copyright © 2018 Ahmad Hobby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
